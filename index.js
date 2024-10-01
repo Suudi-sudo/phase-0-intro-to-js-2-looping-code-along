b50df4aa7e8865b2336fa70e502e1290482f3442
suudi abdisalan <abdisalansucdi@gmail.com>
2024-10-01 16:29:19 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(namesArray, eventName){
+    let thankYouMessages = [];
+    for (let i = 0; i < namesArray.length; i++){
+        thankYouMessages.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return thankYouMessages;
+}
+function countDown(startinggNumber){
+    while (startinggNumber >= 0){
+        console.log(startinggNumber);
+        startinggNumber--;
+        
+    }
+}
